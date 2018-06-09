1ac743314e54388d97923b073889e334e5f62a15
Semih MEMIS <semihmemis67@gmail.com>
2018-06-09 16:50:21 +0300

    Counter Up Placeholder a doğru değerler yazıldı

--- ZHelper/ZHelper.js
+++ ZHelper/ZHelper.js
@@ -608,8 +608,8 @@ $(function () {
 
 		//0-optName, 1-type, 2-name, 3-action, 4-value, 5-placeholder, 6-live_update, 7-slider_def_unit, 8-file_picker, 9-file_picker_no_proxy, 10-show_if, 11-parent, 12-valueisarray, 13-options, 14-show_empty, 15-multiple, 16-customattribute, 17-attributeisvalue, 18-attribute, 19-emptyattribute
 		var counterupattributeoptions = [
-			["time", "slider", "Time", "custom", "2000", "2000", false, "", false, false, "", "", false, null, true, false, "", false, "", false],
-			["delay", "slider", "Delay", "custom", "500", "500", false, "", false, false, "", "", false, null, true, false, "", false, "", false]
+			["time", "slider", "Time", "custom", "1000", "1000", false, "", false, false, "", "", false, null, true, false, "", false, "", false],
+			["delay", "slider", "Delay", "custom", "10", "10", false, "", false, false, "", "", false, null, true, false, "", false, "", false]
 		];
 		//name, classStr, pgStr, dataStr, opts) 
 		creator("Counter Up", "counterup", "counterup", "data-counterup-options", counterupattributeoptions);
