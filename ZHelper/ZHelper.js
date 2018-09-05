28c374962d53568df7e613a9f8e39f44ebbf2633
ps3al <ps3al2018@gmail.com>
2018-09-05 14:20:03 +0300

    AniJs Data to Code

--- ZHelper/ZHelper.js
+++ ZHelper/ZHelper.js
@@ -720,7 +720,7 @@ $(function () {
 
 		//0-optName, 1-type, 2-name, 3-action, 4-value, 5-placeholder, 6-live_update, 7-slider_def_unit, 8-file_picker, 9-file_picker_no_proxy, 10-show_if, 11-parent, 12-valueisarray, 13-options, 14-show_empty, 15-multiple, 16-customattribute, 17-attributeisvalue, 18-attribute, 19-emptyattribute
 		var anijsattributeoptions = [
-			["data", "text", "Data", "custom", "if: click, on:h1, do: pulse animated, to:h2", "if: click, on:h1, do: pulse animated, to:h2", false, "", false, false, "", "", false, null, true, false, "", true, "", false]
+			["code", "text", "Code", "custom", "if: click, on:h1, do: pulse animated, to:h2", "if: click, on:h1, do: pulse animated, to:h2", false, "", false, false, "", "", false, null, true, false, "", true, "", false]
 		];
 		//name, selectorStr, pgStr, dataStr, opts) 
 		creator("AniJS", ".anijs", "anijs", "data-anijs", anijsattributeoptions);
